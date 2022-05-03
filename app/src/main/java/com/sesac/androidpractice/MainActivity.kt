2d38070e5aaeb7240bf5ba58c9cc514d63c61046
jungikim <2015110768@dgu.ac.kr>
2022-05-03 10:40:17 +0900

    Lateinit Binding

--- app/src/main/java/com/sesac/androidpractice/MainActivity.kt
+++ app/src/main/java/com/sesac/androidpractice/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import com.sesac.androidpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-
-    val binding = ActivityMainBinding.inflate(layoutInflater)
+    binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+    /* val binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root) */
 
     val model: MainViewModel by viewModels()
     binding.button.setOnClickListener {
